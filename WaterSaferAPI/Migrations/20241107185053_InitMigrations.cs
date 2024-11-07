409b2b289f5b01528afc8805b25994e8a483f35d
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-11-07 20:03:42 -0300

    ✨ feat: add integration card in watertank endpoint

---
+++ WaterSaferAPI/Migrations/20241107185053_InitMigrations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WaterSaferAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WaterLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Quantity = table.Column<float>(type: "REAL", nullable: false),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WaterLog", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WaterTank",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Volume = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WaterTank", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WaterLog");
+
+            migrationBuilder.DropTable(
+                name: "WaterTank");
+        }
+    }
+}
